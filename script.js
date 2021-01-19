7ed72a31a0e450216a865b1865127d22697df0f9
Albert Portnoy <albert@asportnoy.com>
2021-01-18 18:40:50 -0600

    first
    ooga boogs

---
+++ script.js
@@ -0,0 +1,31 @@
+function getShardNo(guild, shards) {
+	return (parseInt(guild) / Math.pow(2, 22)) % parseInt(shards)
+}
+
+let guildEl = document.getElementById('guild');
+let shardEl = document.getElementById('shards');
+let outputEl = document.getElementById('output');
+
+let url = new URL(window.location.href);
+let params = new URLSearchParams(url.search);
+
+if (params.get('shards')) shardEl.value = params.get('shards');
+if (params.get('guild')) guild.value = params.get('guild');
+
+guildEl.addEventListener('input', update)
+shardEl.addEventListener('input', update)
+
+function update() {
+    let guild = guildEl.value;
+    let shards = shardEl.value;
+    let shardNo = getShardNo(guild, shards);
+    if (shardNo % 1 !== 0) {
+        outputEl.setAttribute('class', 'error');
+        outputEl.innerHTML = 'Invalid input.';
+        return;
+    }
+    outputEl.setAttribute('class', '');
+    outputEl.innerHTML = `Server ${guild} is in shard #${shardNo}.`
+}
+
+if (guildEl.value !== '' && shardEl.value !== '') update();
